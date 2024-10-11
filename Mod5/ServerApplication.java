48d4364260b2fc8f13c8587b37c88f199c11cfbf
Kubia-Beta <113896543+Kubia-Beta@users.noreply.github.com>
2024-10-10 18:30:22 -0700

    Update ServerApplication.java

--- Mod5/ServerApplication.java
+++ Mod5/ServerApplication.java
@@ -6,6 +6,8 @@ import java.security.NoSuchAlgorithmException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,31 +31,40 @@ class ServerController{
 	private static final Logger logger = LoggerFactory.getLogger(ServerController.class); // Spin up slf4j logger
 	
 	@RequestMapping("/hash")
-	public String myHash(){
-		final String data = "Connor Sculthorpe"; // My name to hash
+	public ResponseEntity<String> myHash(){
+		final String data = "Connor Sculthorpe"; // My name to hash 
 		String dataHash = null;
+		
+		if (data == null) { // Blacklist
+			logger.error("Data to hash is null");
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST) // https://spring.io/guides/tutorials/rest
+					.body("<p>Error 400: Data to hash cannot be null</p>");
+		}
+		
 		try {
 			dataHash = generateHash(data); // call our hashing function
 		} catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
 			logger.error("Something went wrong with calling the hash function", e); // Log the error if one is thrown
+			ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+				.body("<p>Error 500: Error occurred while calling the hash function</p>");
 		} 
 		// Our hash should be:
 		// 8d3825ea8fad0009635a6f609e445df83fe85e44f9c848c8f4a876613ad0360c4533dee6aa693de895c800b5c8b3b382
 		// Thanks to: https://emn178.github.io/online-tools/sha384.html
 		
-		return "<p>data: " + data + "</p><p>SHA-384 : CheckSum Value: " + dataHash + "</p>";
+		return ResponseEntity.status(HttpStatus.OK)
+				.body("<p>data: " + data + "</p><p>SHA-384 : CheckSum Value: " + dataHash + "</p>");
 	}
 	
 	/**
 	 * Generates a SHA-384 hash from an input string.
 	 *
-	 * Time: O(n), θ(n), Ω(n)
+	 * Time: O(n), θ(1), Ω(1)
 	 * Space: O(n+k)
 	 * @param String data
 	 * @return String dataHashed
 	 * @throws NoSuchAlgorithmException, UnsupportedEncodingException 
 	 * References: https://stackoverflow.com/questions/5531455/how-to-hash-some-string-with-sha-256-in-java
-	 *             https://www.researchgate.net/publication/364282581_Time_Complexity_Analysis_and_Comparison_of_SHA_Algorithms
 	 */
 	private String generateHash(String data) throws NoSuchAlgorithmException, UnsupportedEncodingException {
 		final MessageDigest md = MessageDigest.getInstance("SHA-384"); // Create message digest obj
